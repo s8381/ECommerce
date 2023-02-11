e1887d13abede8fb8f3d8154ea70dca0d39a6195
tpircsnoitcA <s8381@DESKTOP-24UMPVF.localdomain>
2023-02-11 22:57:38 +0800

    enhanced ProductList component and updated ProductService

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -17,6 +17,16 @@ export class ProductService {
   getProductList(theCategoryId: number): Observable<Product[]>{
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
+    return this.getProducts(searchUrl);
+  }
+
+  searchProducts(keyword: string): Observable<Product[]> {
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${keyword}`;
+
+    return this.getProducts(searchUrl);
+  }
+
+  private getProducts(searchUrl: string): Observable<Product[]> {
     return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
